b3449182abf750afceb072a63c8d01b606b66833
tejaswini3103 <143529757+tejaswini3103@users.noreply.github.com>
2024-01-12 10:35:20 +0530

    Update strmlit.py

--- strmlit.py
+++ strmlit.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('BTS')
 st.write('BTS')
-st.image('https://www.google.com/search?q=bts+cartoon&sca_esv=597709555&rlz=1C1RXQR_enIN1023IN1023&tbm=isch&sxsrf=ACQVn08fkG4Oudc6BxYkOr-w_PXh_b4o3A:1705034779511&source=lnms&sa=X&ved=2ahUKEwi26PLChdeDAxWe-DgGHTuHCZsQ_AUoAXoECAIQAw&biw=1536&bih=695&dpr=1.25#imgrc=M0lhPab9Jf_QdM',caption='BTS')
+st.image('https://img.freepik.com/premium-vector/bts-army_677373-6.jpg',caption='BTS')
